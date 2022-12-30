bc31a5aab9460704e7de32919173c2e3908bef60
lolmageap <ekxk1234@gmail.com>
2022-12-30 13:45:37 +0900

    first commit

---
+++ src/main/java/com/example/fastcampusmysql/domain/member/repository/MemberRepository.java
@@ -0,0 +1,111 @@
+package com.example.fastcampusmysql.domain.member.repository;
+
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class MemberRepository {
+    static final String TABLE = "Member";
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+//     BeanPropertyRowMapper은 사용하면 별도 맵퍼가 필요 없지만, 기본생성자 + setter를 이용해서 만들기때문에 final 필드를 열어줘야함
+//    private final BeanPropertyRowMapper<Member> mapper = BeanPropertyRowMapper.newInstance(Member.class);
+
+    public Member save(Member member){
+        /*
+        member id를 보고 갱신 또는 삽입을 정함
+        반환값은 id를 담아서 반환
+         */
+        if (member.getId() == null){
+            return insert(member);
+        }
+        return update(member);
+    }
+
+    private Member insert(Member member){
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+                .withTableName("Member")
+                .usingGeneratedKeyColumns("id");
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        var id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return Member.builder()
+                .id(id)
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .birthday(member.getBirthday())
+                .createdAt(member.getCreatedAt()).build();
+    }
+
+    private Member update(Member member){
+        // TODO : implemented
+        return member;
+    }
+
+    private static final RowMapper<Member> ROW_MAPPER = (ResultSet resultSet, int rowNum) -> Member.builder()
+            .id(resultSet.getLong("id"))
+            .nickname(resultSet.getString("nickname"))
+            .email(resultSet.getString("email"))
+            .birthday(resultSet.getObject("birthday", LocalDate.class))
+            .createdAt(resultSet.getObject("createdAt", LocalDateTime.class))
+            .build();
+
+    public Optional<Member> findById(Long id) {
+        var sql = String.format("SELECT * FROM %s WHERE id = :id ", TABLE);
+        var params = new MapSqlParameterSource()
+                .addValue("id", id);
+        List<Member> members = namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+
+        // jdbcTemplate.query의 결과 사이즈가 0이면 null, 2 이상이면 예외
+        Member nullableMember = DataAccessUtils.singleResult(members);
+        return Optional.ofNullable(nullableMember);
+    }
+
+    public List<Member> findAllByIdIn(List<Long> ids) {
+        if (ids.isEmpty()) {
+            return List.of();
+        }
+
+        String sql = String.format("SELECT * FROM %s WHERE id in (:ids)", TABLE);
+        var params = new MapSqlParameterSource().addValue("ids", ids);
+        return namedParameterJdbcTemplate.query(sql, params, ROW_MAPPER);
+    }
+
+//    private Member insert(Member member) {
+//        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate())
+//                .withTableName(TABLE)
+//                .usingGeneratedKeyColumns("id");
+//
+//        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+//        var id = jdbcInsert.executeAndReturnKey(params).longValue();
+//
+//        return Member.builder()
+//                .id(id)
+//                .nickname(member.getNickname())
+//                .email(member.getEmail())
+//                .birthday(member.getBirthday())
+//                .build();
+//    }
+//
+//    private Member update(Member member) {
+//        var sql = String.format("UPDATE %s set email = :email, nickname = :nickname, birthday = :birthday WHERE id = :id", TABLE);
+//        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+//        namedParameterJdbcTemplate.update(sql, params);
+//        return member;
+//    }
+}
